c55008c797525ac48c2b833aedaf7e4f44c3e53c
tajohnson0316 <armanijohnson1331@gmail.com>
2023-11-21 20:54:56 -0600

    added create method to user model

--- flask_app/controllers/users_controller.py
+++ flask_app/controllers/users_controller.py
@@ -1,8 +1,16 @@
-from flask import render_template  # request, redirect, session
+from flask import render_template, request, redirect, session
 
 from flask_app import app
+from flask_app.models.user_model import User
 
 
 @app.route("/", methods=["GET"])
 def display_login_registration():
     return render_template("login_registration.html")
+
+
+@app.route("/users/new", methods=['POST'])
+def register_new_user():
+    user_id = User.create_user(request.form)
+
+    return redirect("/")
---
+++ flask_app/models/user_model.py
@@ -0,0 +1,25 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import DATABASE
+
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.list_of_users = []
+
+    @classmethod
+    def create_user(cls, data):
+        query = """
+        INSERT INTO users (first_name, last_name, email, password)
+        VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);
+        """
+
+        result = connectToMySQL(DATABASE).query_db(query, data)
+
+        return result
